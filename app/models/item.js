41b39886786253db1c88ad42c8f077324df75c92
JoAnn Brooks <joannbrooks036@gmail.com>
2014-07-23 10:54:11 -0500

    constructor

---
+++ app/models/item.js
@@ -0,0 +1,13 @@
+'use strict'; 
+
+// var request = require('request');
+
+function Item(name, room, acquired, count, cost) {
+  this.name = name;
+  this.room = room;
+  this.acquired = new Date(acquired);
+  this.count = parseInt(count);
+  this.cost = parseFloat(cost);
+}
+
+module.exports = Item;
